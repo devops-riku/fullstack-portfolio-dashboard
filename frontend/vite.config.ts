d67cd0efdceedfc2505edb91dd5c13f4f8735760
Eric Germino <166575128+devops-riku@users.noreply.github.com>
2026-03-08 00:18:04 +0800

    Update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
 import { fileURLToPath } from "url"
 
 export default defineConfig({
   plugins: [react()],
 
   resolve: {
-    alias: [
-      { find: "@", replacement: fileURLToPath(new URL("./src", import.meta.url)) }
-    ],
+    alias: {
+      "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
+    },
     extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
   },
 
