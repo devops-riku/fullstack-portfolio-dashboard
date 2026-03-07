a31998d643eb961569d9f0cbffe9234c8ea6e91c
Eric Germino <166575128+devops-riku@users.noreply.github.com>
2026-03-08 00:04:00 +0800

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,11 +8,9 @@ if [ ! -f /project_root/.env ]; then
     cp /project_root/.env.example /project_root/.env
 fi
 
-# 2. Sync it to the backend folder so Pydantic can read it
-if [ ! -f /app/.env ]; then
-    echo "Syncing .env to backend folder..."
-    cp /project_root/.env /app/.env
-fi
+# 2. Always sync it to the backend folder so Pydantic is guaranteed to use it
+echo "Syncing .env to backend folder..."
+cp /project_root/.env /app/.env
 
 # Execute the main command passed in from docker-compose
 exec "$@"
