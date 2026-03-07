79ccc40f1edce538214b4c09f243c1ea6a1db9fd
Eric Germino <166575128+devops-riku@users.noreply.github.com>
2026-03-07 19:48:11 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,34 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from "node:url"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+
   server: {
+    host: true,
+    port: 5173,
+
     allowedHosts: [
-      'eric.linkerx.dev'
+      "eric.linkerx.dev"
     ],
+
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
+      "/api": {
+        target: "http://backend:8000", // docker service name
         changeOrigin: true,
-      }
+        secure: false,
+      },
+    },
+
+    watch: {
+      usePolling: true // fixes docker file watching
     }
-  }
+  },
 })
